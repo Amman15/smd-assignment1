daa666e5665a5f8b5379452fa19556943b2e0ce3
Amman15 <98972215+Amman15@users.noreply.github.com>
2023-09-19 23:48:46 +0500

    Update the cricketer class to interface and added the bowler and batsman interface

--- q1.kt
+++ q1.kt
@@ -1,10 +1,41 @@
-open class Cricketer(val name: String, val age: Int, val nationality: String) {
+interface Cricketer
+{
+    val name: String
+    val age: Int
+    val nationality: String
     open fun print() {
-        println("Name: $name, Age: $age, Nationality: $nationality")
+        println("Name of cricketer: $name")
+        println(" Age: $age")
+        println("Nationality: $nationality")
+    }
+}
+interface Bowler : Cricketer
+{
+    var bowlerRanking : Int
+    var wickets : Int
+    open fun getRanking(): Int {
+        return bowlerRanking
+    }
+    open override fun print() {
+        println("Wickets taken: $wickets")
+        println("Bowler Ranking: $bowlerRanking")
+    }
+}
+interface Batsman : Cricketer
+{
+    var batsmanRanking : Int
+    var score: Int
+    open fun getRanking(): Int {
+        return batsmanRanking
+    }
+    open override fun print() {
+        super.print()
+        println("Runs scored: $score")
+        println("Batter Ranking: $batsmanRanking")
     }
 }
 
-fun main(){
-    val Player = Cricketer("Babar", 28, "Pakistani")
-    Player.print()
+fun main()
+{
+
 }
